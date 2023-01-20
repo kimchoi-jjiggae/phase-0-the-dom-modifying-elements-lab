a80f0c61d8f70fdbc825eb9d1a0b95b2464a221d
Michelle <choi.michelle.12@gmail.com>
2023-01-20 16:46:52 -0500

    passes tests

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = 'Michelle is the champion';
+
+document.body.append(newHeader);
